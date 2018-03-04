c1492382000848499c8c7957156f8ded14e2be40
=bluecakewithcows <mason.kohls1999@gmail.com>
2018-03-04 10:11:36 -0800

    Added some new gen. Glitch with lighting, glitch with offset on tile labels

--- src/core/GeneratorSet.java
+++ src/core/GeneratorSet.java
@@ -4,6 +4,7 @@ import levelBuilder.Generator;
 import levelBuilder.GeneratorBag;
 import levelBuilder.Player;
 import levelBuilder.World;
+import levelBuilder.generators.QuantumDrunkard;
 import levelBuilder.generators.DungeonGenerator;
 import tileEngine.Tileset;
 
@@ -17,7 +18,10 @@ public class GeneratorSet {
         public World generate(long l, int depth, Player player) {
             switch (depth) {
                 case 1:
-                    break;
+                    return quantumWalk.generate(l, player,
+                        Map.ofEntries(entry("width", 100), entry("height", 100),
+                            entry("floor_tile", Tileset.FLOOR), entry("wall_tile1", Tileset.WALL),
+                            entry("wall_tile2", Tileset.WALL2)));
                 case 2:
                     break;
                 default:
@@ -38,4 +42,6 @@ public class GeneratorSet {
         new DungeonGenerator("Default Generator", .3, 1, .01, 12, 2.5, 4, 3, 10,
             new int[]{100, 100}, 50, 50);
 
+
+    private static final Generator quantumWalk = new QuantumDrunkard("Quantum Generator", 100, 100);
 }
